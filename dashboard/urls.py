cd4bfad6a7dde44689dd7359026ffac16a99f45f
Devyani030 <raidevyani3@gmail.com>
2023-09-12 14:53:29 +0530

    login-logout done

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,14 +1,17 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.decorators import login_required
 
 app_name = "dashboard"
 urlpatterns = [
-    path('',views.DashboardView.as_view(), name='message'),
-    path("message/<int:pk>/", views.ReceivedMessage.as_view(), name="received_messages"),
-    path('<int:pk>/delete/', views.delete_info,name="delete_messages"),
-    path('blog/',views.BlogView.as_view(), name='blog_view'),
-    path("contents/<int:pk>/", views.Blogcontent.as_view(), name="contents"),
-    path("addblog/", views.add_blog, name="new_blog"),
-    path('<str:pk>/delete_items/', views.delete_blog, name="delete_blog"),
-    path('update/<int:pk>' , views.update_blog, name="update"),
+    path('',login_required(views.DashboardView.as_view()), name='message'),
+    path("message/<int:pk>/", login_required(views.ReceivedMessage.as_view()), name="received_messages"),
+    path('<int:pk>/delete/', login_required(views.delete_info),name="delete_messages"),
+    path('blog/',login_required(views.BlogView.as_view()), name='blog_view'),
+    path("contents/<int:pk>/", login_required(views.Blogcontent.as_view()), name="contents"),
+    path("addblog/", login_required(views.add_blog), name="new_blog"),
+    path('<str:pk>/delete_items/', login_required(views.delete_blog), name="delete_blog"),
+    path('update/<int:pk>' , login_required(views.update_blog), name="update"),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
--- website/urls.py
+++ website/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .views import index, HomePageView, BlogDetailView
+
+
 app_name = 'website'
 urlpatterns = [
     path('', HomePageView.as_view(), name = "index"),
     path('blog/<int:pk>', BlogDetailView.as_view(), name = "blog_view"),
+    
 ]
